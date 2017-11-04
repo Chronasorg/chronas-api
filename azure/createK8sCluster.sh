dca77aa0b4f77a2d7a3fda1dec616e34d112d6dc
Joachim Aumann <joachimaumann@Joachims-Air.fritz.box>
2017-11-05 00:03:10 +0100

    updated kubernetes

--- azure/createK8sCluster.sh
+++ azure/createK8sCluster.sh
@@ -1,5 +1,8 @@
-az group create --name kubanet --location westus2
-az aks create --resource-group kubanet --name myK8sCluster --agent-count 1 --generate-ssh-keys
+az group create --name chronas-k8s --location westus2
+az aks create --resource-group chronas-k8s --name myK8sCluster --agent-count 1 --generate-ssh-keys
+
+az aks get-credentials --resource-group=chronas-k8s --name=myK8sCluster
+
 
 az group delete --name kubanet --yes
 
