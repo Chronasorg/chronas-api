9b84f4f196c56e505152613ef7006d3f722ebd46
Joachim Aumann <joachimaumann@Joachims-Air.fritz.box>
2017-11-05 23:46:33 +0100

    fixed issue with mongo port

--- azure/createK8sCluster.sh
+++ azure/createK8sCluster.sh
@@ -1,10 +1,22 @@
+
+#create westus
 az group create --name chronas-k8s --location westus2
 az aks create --resource-group chronas-k8s --name myK8sCluster --agent-count 1 --generate-ssh-keys
 
+
+#create ukwest
+az group create --name chronas-k8s --location ukwest
+az aks create --resource-group chronas-k8s --name myK8sCluster --agent-count 1 --generate-ssh-keys -s Standard_D1_v2
+
+
+
+#get credentials
 az aks get-credentials --resource-group=chronas-k8s --name=myK8sCluster
+#dashbaord
+az aks browse --resource-group chronas-k8s --name myK8sCluster
 
 
-az group delete --name kubanet --yes
+az group delete --name chronas-k8s --yes
 
 kubectl cluster-info
 
