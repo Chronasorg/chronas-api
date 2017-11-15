2572125667ca601109acca2902d55a85c0efe0af
Joachim Aumann <joachimaumann@Joachims-Air.fritz.box>
2017-11-15 22:56:55 +0100

    changed location to westeurope

--- azure/createK8sCluster.sh
+++ azure/createK8sCluster.sh
@@ -1,8 +1,8 @@
 
-#create westus
-az group create --name chronas-k8s --location eastus
+#create westeu
+az group create --name chronas-k8s --location westeu
 az aks create --resource-group chronas-k8s --name myK8sCluster --agent-count 1 --generate-ssh-keys
-az aks get-credentials --resource-group=chronas-k8s --name=myK8sCluster
+az aks get-credentials --resource-group=chronas-k8s --name=myK8sCluster -f ./myk8sclusterConfig
 
 
 #create ukwest - not working
