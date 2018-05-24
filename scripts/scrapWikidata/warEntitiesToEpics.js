a586bf52af4f429f2d5685d01a3c7123cd926a5a
Dietmar Aumann <dietmar.aumann@gmail.com>
2018-05-23 21:30:04 -0500

    wip

--- scripts/scrapWikidata/warEntitiesToEpics.js
+++ scripts/scrapWikidata/warEntitiesToEpics.js
@@ -4,16 +4,9 @@ const properties = {
   chronasApiHost: 'http://localhost:4040/v1'
 }
 
-// https://commons.wikimedia.org/w/api.php?action=opensearch&search=Flag%20of%20Nigeria&namespace=*&profile=fuzzy&limit=500
+// https://query.wikidata.org/sparql?format=json&query=SELECT+DISTINCT+%3Fitem+WHERE+%7B%0A++%3Fitem+wdt:P31+%3Fsub0+.%0A++%3Fsub0+(wdt:P279)*+wd:Q198+.%0A++%3Fitem+wdt:P580+%5B%5D+.%0A%7D
 
-// 1: first banner after flag, if empty with only
-
-// flag of ..
-// banner of ..
-// .. flag
-// .. banner
-
-// with all words and subsets
+// war and subclass (battles sieges etc)
 
 // get correct resolution:
 // https://commons.wikimedia.org/w/api.php?action=query&titles=File:Flag_of_Nigerian_Jews.JPG&prop=imageinfo&&iiprop=url&iiurlwidth=100&format=json
@@ -24,7 +17,7 @@ const properties = {
 const rulList = []
 let originalMeta
 
-fetch(`${properties.chronasApiHost}/metadata/religionGeneral`)
+fetch(`${properties.chronasApiHost}/metadata/ruler`)
   .then(response => response.json())
   .then((rulerMetadata) => {
     originalMeta = rulerMetadata
@@ -52,7 +45,7 @@ fetch(`${properties.chronasApiHost}/metadata/religionGeneral`)
 
 postRulPlus = (rulAcc, rulKey) => new Promise((resolve, reject) => {
   if (rulAcc === "!done") {
-    fetch(`${properties.chronasApiHost}/metadata/religionGeneral`, {
+    fetch(`${properties.chronasApiHost}/metadata/ruler`, {
       method: 'PUT',
       body: JSON.stringify(originalMeta),
       headers: {
@@ -141,21 +134,7 @@ postRulPlus = (rulAcc, rulKey) => new Promise((resolve, reject) => {
                                                     goOn(rulAcc, ruler[1], resolve, reject)
                                                   }
                                                   else {
-                                                    fetch("https://commons.wikimedia.org/w/api.php?action=opensearch&search=" + firstWord + "&namespace=*&profile=fuzzy&limit=500")
-                                                      .then(response => response.json())
-                                                      .then((ruler) => {
-                                                        rulerImagesString = JSON.stringify(ruler[1]).toLowerCase()
-                                                        if (ruler[1].length > 0 && (rulerImagesString.indexOf(".svg") > -1 || rulerImagesString.indexOf(".jpg") > -1 || rulerImagesString.indexOf(".jpeg") > -1 || rulerImagesString.indexOf(".png") > -1)) {
-                                                          // we found something valid, go on
-                                                          goOn(rulAcc, ruler[1], resolve, reject)
-                                                        }
-                                                        else {
-                                                          resolve()
-                                                        }
-                                                      })
-                                                      .catch((err) => {
-                                                        resolve()
-                                                      })
+                                                    resolve()
                                                   }
                                                 })
                                                 .catch((err) => {
@@ -211,15 +190,103 @@ goOn = (rulAcc, imageArray, resolve, reject) => {
           const startUrl = thumbUrl.indexOf("commons/thumb/") + "commons/thumb/".length
           const endUrl = thumbUrl.substr(52).lastIndexOf("/") + startUrl -1
           thumbUrl = thumbUrl.substring(startUrl, endUrl)
-          originalMeta.data[rulAcc][3] = thumbUrl
+
+          originalMeta.data[rulAcc][3] = thumbUrl)
           console.debug(rulAcc + " -> " + originalMeta.data[rulAcc][3])
           resolve()
         })
         .catch((err) => {
-          originalMeta.data[rulAcc][3] = undefined
           resolve()
         })
       break
     }
   }
 }
+
+// example output
+/*
+{
+  "_id": "e_Byzantine–Sasanian_War_of_602–628",
+  "data": {
+  "title": "Byzantine–Sassanid War of 602–28",
+    "wiki": "Byzantine–Sasanian_War_of_602–628",
+    "start": "602",
+    "end": "628",
+    "participants": [
+    [
+      "SAS",
+      "AVR"
+    ],
+    [
+      "BYZ",
+      "WGO"
+    ]
+  ],
+    "content": [
+    {
+      "wiki": "Jewish_revolt_against_Heraclius",
+      "type": "conflict"
+    },
+    {
+      "wiki": "Battle_of_Antioch_(613)",
+      "type": "battle",
+      "date": "613"
+    },
+    {
+      "wiki": "Sasanian conquest of Jerusalem",
+      "type": "siege",
+      "date": "614"
+    },
+    {
+      "wiki": "Shahin's invasion of Asia Minor (615)",
+      "type": "conflict",
+      "date": "615"
+    },
+    {
+      "wiki": "Heraclius' campaign of 622",
+      "type": "campaign",
+      "date": "622"
+    },
+    {
+      "wiki": "Siege of Constantinople (626)",
+      "type": "siege",
+      "date": "June 626"
+    },
+    {
+      "wiki": "Battle of Nineveh",
+      "type": "battle",
+      "date": "12 December 627"
+    }
+  ]
+},
+  "wiki": "Byzantine–Sasanian_War_of_602–628",
+  "subtype": "war",
+  "year": 602,
+  "score": 0,
+  "linked": [
+  "Jewish_revolt_against_Heraclius",
+  "Battle_of_Antioch_(613)",
+  "Sasanian conquest of Jerusalem",
+  "Shahin's invasion of Asia Minor (615)",
+  "Heraclius' campaign of 622",
+  "Siege of Constantinople (626)",
+  "Battle of Nineveh"
+],
+  "type": "e"
+}
+
+
+
+call
+https://query.wikidata.org/sparql?format=json&query=SELECT+DISTINCT+%3Fitem+WHERE+%7B%0A++%3Fitem+wdt:P31+%3Fsub0+.%0A++%3Fsub0+(wdt:P279)*+wd:Q198+.%0A++%3Fitem+wdt:P580+%5B%5D+.%0A%7D
+to get list of wars and subclasses,
+
+then iterate over them creating the above object + an optional partOf field (for battles, sieges linking to wars for example)
+
+iteration url = https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=Q5778798&languages=en&props=sitelinks|claims
+
+part of (P361)
+instance of (P31)
+has part (P527)
+
+*/
