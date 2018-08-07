ec6a1d075e312cd574ca93376370924bd2a2734c
Joachim Aumann <joachimaumann@Joachims-Air.fritz.box>
2018-08-07 14:10:18 +0200

    fixed cert issue

--- infrastructure/kubernetes/cert-manager/installCertManager.sh
+++ infrastructure/kubernetes/cert-manager/installCertManager.sh
@@ -3,7 +3,12 @@ kubectl create serviceaccount -n kube-system tiller
 kubectl create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller
 helm init --service-account tiller
 helm repo update
-helm install --name my-release stable/cert-manager --set rbac.create=false
+helm install \
+    --name cert-manager \
+    --namespace kube-system \
+    stable/cert-manager
+
+sleep 100
 
 #PWD as a hack to call it from parent script
 kubectl create -f $PWD/kubernetes/cert-manager/letsencryp-clusterissuer-prod.yaml
