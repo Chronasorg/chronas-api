9f544f776433a75703cdaddeb8859c0ad7249131
Joachim Aumann <joachimaumann@Joachims-Air.fritz.box>
2018-08-07 09:26:44 +0200

    updated script that they run in one flow

--- infrastructure/kubernetes/cert-manager/installCertManager.sh
+++ infrastructure/kubernetes/cert-manager/installCertManager.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 kubectl create serviceaccount -n kube-system tiller
 kubectl create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller
 helm init --service-account tiller
+helm repo update
 helm install --name my-release stable/cert-manager --set rbac.create=false
-kubectl create -f letsencryp-clusterissuer-prod.yaml
-kubectl create -f chronas-api-certificate.yml
+
+#PWD as a hack to call it from parent script
+kubectl create -f $PWD/kubernetes/cert-manager/letsencryp-clusterissuer-prod.yaml
+kubectl create -f $PWD/kubernetes/cert-manager/chronas-api-certificate.yml
