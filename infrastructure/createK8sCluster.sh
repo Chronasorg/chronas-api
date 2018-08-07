74459bb22a7cbb56b9faed4a9118d1a5d01641d2
Joachim Aumann <joachimaumann@Joachims-Air.fritz.box>
2018-08-07 09:26:44 +0200

    updated script that they run in one flow

--- infrastructure/createK8sCluster.sh
+++ infrastructure/createK8sCluster.sh
@@ -4,15 +4,17 @@ cosmosDbName='chronasapi'
 aksClusterName='chronask8sdev'
 #create cluster in westeu
 az group create --name $resourceGroupName --location westeurope
-az aks create --resource-group $resourceGroupName --name $aksClusterName --node-count 2 --enable-addons monitoring --generate-ssh-keys -s Standard_DS1_v2 --kubernetes-version 1.10.6
-az aks get-credentials --resource-group $resourceGroupName --name $aksClusterName
+#az aks create --resource-group $resourceGroupName --name $aksClusterName --node-count 2 --enable-addons monitoring --generate-ssh-keys -s Standard_DS1_v2 --kubernetes-version 1.10.6
+#az aks get-credentials --resource-group $resourceGroupName --name $aksClusterName
 
-#create ingress
-source ingress/deployIngress.ssh
+echo 'create ingress'
+bash $PWD/kubernetes/ingress/deployIngress.sh
 
-#create lets encrypt cert manager
+echo 'create lets encrypt cert manager'
+bash $PWD/kubernetes/cert-manager/installCertManager.sh
 
-source cert-manager/installCertManager.sh
+echo 'provision mongodb'
+kubectl apply -f $PWD/kubernetes/mongo/mongo-statefulset.yml
 
 #az cosmosdb create \
 #        --name $cosmosDbName \
--- infrastructure/kubernetes/ingress/deployIngress.sh
+++ infrastructure/kubernetes/ingress/deployIngress.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
 
-kubectl apply -f chronas-api-ingress.yml
+#PWD as hack to execute it from parent file
+kubectl apply -f $PWD/kubernetes/ingress/chronas-api-ingress.yml
