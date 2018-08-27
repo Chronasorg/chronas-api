185608e2aeebea392c5fd004d02a4dcb11aee2c4
Joachim Aumann <aumannjoachim@gmail.com>
2018-08-27 13:41:20 +0200

    Update deployIngress.sh

--- infrastructure/kubernetes/ingress/deployIngress.sh
+++ infrastructure/kubernetes/ingress/deployIngress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm install stable/nginx-ingress --namespace kube-system --set rbac.create=false --set controller.service.externalTrafficPolicy=Local
+helm install stable/nginx-ingress --namespace kube-system --set controller.service.externalTrafficPolicy=Local
 
 #PWD as hack to execute it from parent file
 kubectl apply -f $PWD/kubernetes/ingress/chronas-api-ingress.yml
