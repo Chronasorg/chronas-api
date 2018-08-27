acf51cb506c1afe243d03b7c7d8f2203ee4165f9
Joachim Aumann <aumannjoachim@gmail.com>
2018-08-27 13:36:26 +0200

    went to latest ingress version installing via helm

--- infrastructure/kubernetes/ingress/deployIngress.sh
+++ infrastructure/kubernetes/ingress/deployIngress.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+helm install stable/nginx-ingress --namespace kube-system --set rbac.create=false --set controller.service.externalTrafficPolicy=Local
 
 #PWD as hack to execute it from parent file
 kubectl apply -f $PWD/kubernetes/ingress/chronas-api-ingress.yml
