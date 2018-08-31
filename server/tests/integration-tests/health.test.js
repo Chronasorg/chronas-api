ad84414eb9f0737c9ed2bc149d5f3760de5bd7e7
Joachim Aumann <joachim.aumann@sivantos.com>
2018-08-31 13:45:22 +0200

    added user tests

---
+++ server/tests/integration-tests/health.test.js
@@ -0,0 +1,38 @@
+import request from 'supertest-as-promised'
+import httpStatus from 'http-status'
+import chai, { expect } from 'chai'
+import app from '../../..'
+import mongoUnit from 'mongo-unit'
+
+chai.config.includeStack = true
+
+describe('## health', () => {
+
+  const testMongoUrl = process.env.MONGO_HOST
+  const testData = require('./fixtures/testData.json')  
+
+  before(() => mongoUnit.initDb(testMongoUrl, testData))
+  after(() => mongoUnit.drop())
+
+    it('should return OK', (done) => {
+      request(app)
+        .get('/v1/health')
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.text).to.equal('OK')
+          done()
+        })
+        .catch(done)
+    })
+
+    it('should return 404 status', (done) => {
+      request(app)
+        .get('/v1/404')
+        .expect(httpStatus.NOT_FOUND)
+        .then((res) => {
+          expect(res.body.message).to.equal('Not Found')
+          done()
+        })
+        .catch(done)
+    })
+})
--- server/tests/integration-tests/user.test.js
+++ server/tests/integration-tests/user.test.js
@@ -1,65 +1,116 @@
 import request from 'supertest-as-promised'
 import httpStatus from 'http-status'
 import chai, { expect } from 'chai'
-import app from '../../../index'
+import app from '../../..'
 import mongoUnit from 'mongo-unit'
-
-// https://www.toptal.com/nodejs/integration-and-e2e-tests-nodejs-mongodb
+import jwt from 'jsonwebtoken'
 
 chai.config.includeStack = true
 
-describe('## Misc', () => {
+describe('## User APIs', () => {
 
   const testMongoUrl = process.env.MONGO_HOST
-  const testData = require('./fixtures/testData.json')  
+  const testData = require('./fixtures/testData.json')
 
   before(() => mongoUnit.initDb(testMongoUrl, testData))
   after(() => mongoUnit.drop())
 
-    it('should return OK', (done) => {
+  const validUserCredentials = {
+    email: 'test@test.de',
+    password: 'asdf'
+  }
+
+  let user = {
+    username: 'KK123',
+    privilege: 10
+  }
+
+  let jwtToken
+
+  describe('# POST /v1/auth/login', () => {
+    it('should get valid JWT token', (done) => {
       request(app)
-        .get('/v1/health')
+        .post('/v1/auth/login')
+        .send(validUserCredentials)
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.text).to.equal('OK')
-          done()
+          expect(res.body).to.have.property('token')
+          jwt.verify(res.body.token, config.jwtSecret, (err, decoded) => {
+            expect(err).to.not.be.ok // eslint-disable-line no-unused-expressions
+            expect(decoded.username).to.equal(validUserCredentials.username)
+            jwtToken = `Bearer ${res.body.token}`
+            done()
+          })
         })
         .catch(done)
     })
+  })
 
-    it('should return 404 status', (done) => {
-      request(app)
-        .get('/v1/404')
-        .expect(httpStatus.NOT_FOUND)
-        .then((res) => {
-          expect(res.body.message).to.equal('Not Found')
-          done()
-        })
-        .catch(done)
-    })
-
-    it('should handle 404', (done) => {
-      request(app)
-        .get('/v1/users/56z787zzz67fc')
-        .expect(httpStatus.NOT_FOUND)
-        .then((res) => {
-          expect(res.body.message).to.equal('Not Found')
-          done()
-        })
-        .catch(done)
-    })
-
-    it('should handle express validation error - username is required', (done) => {
+  describe('# POST /v1/users', () => {
+    it('should create a new user', (done) => {
       request(app)
         .post('/v1/users')
-        .send({
-          privilege: 'public'
-        })
-        .expect(httpStatus.BAD_REQUEST)
+        .send(user)
+        .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body.message).to.equal('"username" is required')
+          expect(res.body.username).to.equal(user.username)
+          expect(res.body.privilege).to.equal(user.privilege)
+          user = res.body
           done()
         })
         .catch(done)
     })
+  })
+
+  describe('# GET /v1/users/', () => {
+    it('should get all users', (done) => {
+      request(app)
+        .get('/v1/users')
+        .set('Authorization', jwtToken)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.body).to.be.an('array')
+          done()
+        })
+        .catch(done)
+    })
+  })
+
+  describe('# GET /v1/users/ without token', () => {
+    it('should return unauthorized', (done) => {
+      request(app)
+        .get('/v1/users')
+        .expect(httpStatus.UNAUTHORIZED)
+        .then((res) => {
+          expect(res.body.message).to.equal("Unauthorized")
+          done()
+        })
+        .catch(done)
+    })
+  })  
+
+  it('should handle if user not exist 404', (done) => {
+    request(app)
+      .get('/v1/users/56z787zzz67fc')
+      .expect(httpStatus.NOT_FOUND)
+      .then((res) => {
+        expect(res.body.message).to.equal('Not Found')
+        done()
+      })
+      .catch(done)
+  })
+
+  it('should handle express validation error - username is required', (done) => {
+    request(app)
+      .post('/v1/users')
+      .send({
+        privilege: 'public'
+      })
+      .expect(httpStatus.BAD_REQUEST)
+      .then((res) => {
+        expect(res.body.message).to.equal('"username" is required')
+        done()
+      })
+      .catch(done)
+  })
 })
