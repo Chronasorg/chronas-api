aed73229949162857d1201882345c5a0f9d36fa9
Joachim Aumann <joachim.aumann@sivantos.com>
2018-09-04 17:13:57 +0200

    added some more metadata test for put and delete

--- server/tests/integration-tests/metadata.test.js
+++ server/tests/integration-tests/metadata.test.js
@@ -20,25 +20,32 @@ describe('## Metadata APIs', () => {
   }
 
   let metadata = {
-      _id: "religion",
-      data: "{\"south_arabian\":[\"South Arabian\",\"rgb(153,26,51)\",\"Arabian_mythology\",\"Paganism\"],\"ashurism\":[\"Ashurism\",\"rgb(230,230,230)\",\"Ancient_Mesopotamian_religion\",\"Paganism\"],\"african\":[\"African\",\"rgb(204,5",
-      __v: 0,
-      score: 0,
-      type: "g",
-      coo: []
+    _id: "religion",
+    data: "{\"south_arabian\":[\"South Arabian\",\"rgb(153,26,51)\",\"Arabian_mythology\",\"Paganism\"],\"ashurism\":[\"Ashurism\",\"rgb(230,230,230)\",\"Ancient_Mesopotamian_religion\",\"Paganism\"],\"african\":[\"African\",\"rgb(204,5",
+    __v: 0,
+    score: 0,
+    type: "g",
+    coo: []
 
   }
 
-  let updateMarker = {
-    _id: "Mamurra",
-    name: "Mamurra",
-    wiki: "Mamurra",
-    type: "politician",
-    year: -91,
-    coo: [
-      13.616666666667,
-      41.266666666667
-    ]
+  let updateMetadata = {
+    _id: "culture",
+    data: {
+      "sapmiii": [
+        "sapmi",
+        "rgb(157,51,167)",
+        "asdf"
+      ],
+      "samoyed": [
+        "samoyed",
+        "rgb(220,220,103)",
+        "Samoyedic peoples"
+      ]
+    },
+    __v: 1,
+    coo: [],
+    type: "g"
   }
 
   let jwtToken
@@ -60,7 +67,6 @@ describe('## Metadata APIs', () => {
 
     describe('# POST /v1/metadata', () => {
       it('should create a new metadata', (done) => {
-        console.log(jwtToken)
         request(app)
           .post('/v1/metadata')
           .set('Authorization', jwtToken)
@@ -72,7 +78,7 @@ describe('## Metadata APIs', () => {
             done()
           })
           .catch(done)
-      })      
+      })
 
       it('should fail to post metadata because of missing token', (done) => {
         request(app)
@@ -114,65 +120,62 @@ describe('## Metadata APIs', () => {
       })
     })
 
-    // describe('# Put /v1/markers', () => {
+    describe('# Put /v1/metadata', () => {
 
-    //   it('should fail to put markers because of wrong token', (done) => {
-    //     request(app)
-    //       .put('/v1/markers/Mamurra')
-    //       .set('Authorization', 'Bearer inValidToken')
-    //       .expect(httpStatus.UNAUTHORIZED)
-    //       .then((res) => {
-    //         expect(res.body.message).to.equal('Unauthorized')
-    //         done()
-    //       })
-    //       .catch(done)
-    //   })
+      it('should fail to update metadata because of wrong token', (done) => {
+        request(app)
+          .put('/v1/metadata/culture')
+          .set('Authorization', 'Bearer inValidToken')
+          .expect(httpStatus.UNAUTHORIZED)
+          .then((res) => {
+            expect(res.body.message).to.equal('Unauthorized')
+            done()
+          })
+          .catch(done)
+      })
 
-    //   it('should update a marker', (done) => {
-    //     updateMarker.type = 'aui'
-    //     console.log(updateMarker)
-    //     request(app)
-    //       .put(`/v1/markers/${updateMarker._id}`)
-    //       .set('Authorization', jwtToken)
-    //       .send(updateMarker)
-    //       .expect(httpStatus.OK)
-    //       .then((res) => {
-    //         console.log(res.body)
-    //         expect(res.body._id).to.equal(updateMarker._id)
-    //         expect(res.body.type).to.equal('aui')
-    //         done()
-    //       })
-    //       .catch(done)
-    //   })
-    // })
+      it('should update a metadat value', (done) => {
+        request(app)
+          .put(`/v1/metadata/${updateMetadata._id}`)
+          .set('Authorization', jwtToken)
+          .send(updateMetadata)
+          .expect(httpStatus.OK)
+          .then((res) => {
+            expect(res.body._id).to.equal(updateMetadata._id)
+            expect(Object.keys(res.body.data).length).to.equal(2)
+            done()
+          })
+          .catch(done)
+      })
+    })
 
-    // describe('# delete /v1/markers', () => {
+    describe('# delete /v1/metadata', () => {
 
-    //   it('should fail to delete markers because of wrong token', (done) => {
-    //     request(app)
-    //       .delete('/v1/markers/Mamurra')
-    //       .set('Authorization', 'Bearer inValidToken')
-    //       .expect(httpStatus.UNAUTHORIZED)
-    //       .then((res) => {
-    //         expect(res.body.message).to.equal('Unauthorized')
-    //         done()
-    //       })
-    //       .catch(done)
-    //   })
+      it('should fail to delete metadata because of wrong token', (done) => {
+        request(app)
+          .delete('/v1/metadata/culture')
+          .set('Authorization', 'Bearer inValidToken')
+          .expect(httpStatus.UNAUTHORIZED)
+          .then((res) => {
+            expect(res.body.message).to.equal('Unauthorized')
+            done()
+          })
+          .catch(done)
+      })
 
-    //   it('should delete a marker', (done) => {
-    //     request(app)
-    //       .delete('/v1/markers/deleteMamurra')
-    //       .set('Authorization', jwtToken)
-    //       .expect(httpStatus.OK)
-    //       .then((res) => {
-    //         expect(res.body._id).to.equal('deleteMamurra')
-    //         expect(res.body.type).to.equal('politician')
-    //         done()
-    //       })
-    //       .catch(done)
-    //   })
-    // })
+      it('should delete a metadata ', (done) => {
+        request(app)
+          .delete('/v1/metadata/culture')
+          .set('Authorization', jwtToken)
+          .expect(httpStatus.OK)
+          .then((res) => {
+            expect(res.body._id).to.equal('culture')
+            expect(res.body.type).to.equal('g')
+            done()
+          })
+          .catch(done)
+      })
+    })
   })
 
 
