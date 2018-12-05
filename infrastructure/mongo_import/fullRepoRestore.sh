fb34597d6f7f89b6b508eac5e880e376ea42b4ff
Dietmar Aumann <dietmar.aumann@gmail.com>
2018-12-05 15:24:07 -0600

    add last data edit to welcome call

--- infrastructure/mongo_import/fullRepoRestore.sh
+++ infrastructure/mongo_import/fullRepoRestore.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+
+# 0: start restore: kubectl run -i --tty --attach restore --image=aumanjoa/helper:mongorestore &
+
 if [[ $# -eq 0 ]] ; then
     echo "first argument: name of mongodump name placed in your local dump folder, example: dumpNov4"
     echo "second argument: name of local db, example: chronas-api-staging"
     exit 0
 fi
 
+mongodump --db chronas-api-staging -o dump/$1
+
 restorePodName=`kubectl get pod | grep restore | awk '{print $1;}'`
 
-echo "*** using $restorePodName as restore podname, now copy to local dump to pod"
+echo "*** using $restorePodName as restore podname, now copying local dump to pod"
 
 kubectl cp dump/$1 $restorePodName:/home/dump/
 
 echo "*** copy done, now executing the mongorestore on pod"
 
 kubectl exec -it $restorePodName -- bash -c "mongorestore --host mongo-0.mongo,mongo-1.mongo --port 27017 --drop -d chronas-api /home/dump/$1/$2"
+
+kubectl delete deployment restore
--- server/controllers/version.controller.js
+++ server/controllers/version.controller.js
@@ -1,6 +1,9 @@
 import { composePatternSync } from 'appversion'
 import User from '../models/user.model'
 import Revision from '../models/revision.model'
+import Promise from "bluebird";
+import APIError from "../helpers/APIError";
+import httpStatus from "http-status";
 
 
 /**
@@ -22,9 +25,20 @@ function getPlusUser(req, res) {
   const getVersion = composePatternSync('M.m.p')
   const getCommit = composePatternSync('c')
   const buildDate = composePatternSync('d')
-  const formatedDate = new Date(buildDate).toLocaleDateString()
+  const formatedDate = buildDate
 
-  User.count().exec().then(userCount => res.json({ version: getVersion, commit: getCommit, build: formatedDate, user: userCount }))
+  Revision
+    .find()
+    .sort({"timestamp": -1})
+    .limit(1)
+    .lean()
+    .exec()
+    .then((rev) => {
+      User.count().exec().then(userCount => {
+        console.debug(rev)
+        res.json({ lastDataEdit: (rev[0] || {}).timestamp || "n/a", version: getVersion, commit: getCommit, build: formatedDate, user: userCount })
+      })
+    })
 }
 
 export default { get, getPlusUser }
