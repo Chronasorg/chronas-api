a340a5cc9e5325b38c6c90176872bfe107236725
Joachim Aumann <aumannjoachim@gmail.com>
2019-10-29 23:02:52 +0100

    used the new 0.11 cert manager

--- infrastructure/kubernetes/cert-manager/installCertManager.sh
+++ infrastructure/kubernetes/cert-manager/installCertManager.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
-#kubectl create serviceaccount -n kube-system tiller
-#kubectl create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller
 # Install the CustomResourceDefinition resources separately
-kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
 
 # Create the namespace for cert-manager
 kubectl create namespace cert-manager
 
-# Label the cert-manager namespace to disable resource validation
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
 
+# Update your local Helm chart repository cache
 helm repo update
+
+# Install the cert-manager Helm chart
 helm install \
   --name cert-manager \
   --namespace cert-manager \
-  stable/cert-manager
-
-sleep 100
+  --version v0.11.0 \
+  jetstack/cert-manager
