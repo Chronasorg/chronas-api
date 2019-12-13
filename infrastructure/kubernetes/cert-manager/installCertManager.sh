83f087b907afe6c8e51ffff43e9e29d1b5f74de2
Joachim Aumann <aumannjoachim@gmail.com>
2019-12-13 21:42:51 +0100

    moved to newest cert manager

--- infrastructure/kubernetes/cert-manager/installCertManager.sh
+++ infrastructure/kubernetes/cert-manager/installCertManager.sh
@@ -1,18 +1,15 @@
-# Install the CustomResourceDefinition resources separately
-kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
+# https://cert-manager.io/docs/installation/kubernetes/ 
 
-# Create the namespace for cert-manager
-kubectl create namespace cert-manager
-
-# Add the Jetstack Helm repository
+kubectl apply --validate=false -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.12/deploy/manifests/00-crds.yaml
 helm repo add jetstack https://charts.jetstack.io
-
-# Update your local Helm chart repository cache
 helm repo update
 
-# Install the cert-manager Helm chart
 helm install \
   --name cert-manager \
   --namespace cert-manager \
-  --version v0.11.0 \
+  --version v0.12.0 \
   jetstack/cert-manager
+
+  kubectl get pods --namespace cert-manager
+
+  
